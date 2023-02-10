173a99437b19c9d22789f3e675ddad8fdbbbc5bf
wrdwyer <wrdwyer@gmail.com>
2023-02-10 19:07:30 +0000

    Added Rewired controller and added Poly assets

---
+++ Assets/Scripts/FootPrintsSpotted.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootPrintsSpotted : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Foot Prints Spotted!!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
